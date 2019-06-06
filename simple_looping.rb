5d575fa34e6a24baacb7b3217a6ae26cd6695feb
Trejon Stallsworth <trejonstallsworth@gmail.com>
2019-06-06 18:28:19 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -15,6 +15,7 @@ def loop_iterator(number_of_times)
 end
 break if count >= number_of_times
 end
+end 
 def times_iterator(number_of_times)
   # code your solution here using the "times" keyword
   phrase = "Welcome to Flatiron School's Web Development Course!"
