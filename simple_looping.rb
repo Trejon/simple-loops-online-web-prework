babfd6d429164271d7cb8f3dd1040db1c6902901
Trejon Stallsworth <trejonstallsworth@gmail.com>
2019-06-06 18:33:00 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -23,10 +23,8 @@ def times_iterator(number_of_times)
   # code your solution here using the "times" keyword
   phrase = "Welcome to Flatiron School's Web Development Course!"
   
-  count = 0
-  count do 
+  number_of_times do 
     puts phrase 
-    number_of_times += 1 
   end 
 end
 
